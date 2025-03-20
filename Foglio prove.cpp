c2c2de4444bf54f04f5aa7ffdd7c172df77129bb
Alessandro <bisini.alessandro@gmail.com>
2025-03-20 08:31:08 +0100

    modifca per gioco

--- Foglio prove.cpp
+++ Foglio prove.cpp
@@ -3,5 +3,9 @@
 int main (void){
 
     int a=9;
-    printf("prova per vedere se funziona %d", a);
+    printf("prova per vedere se funziona %d\n", a);
+    for (int i=0; i<=3 ;i++)
+    {
+            printf("alle è morto %d volte\n", i);
+    }
 }
